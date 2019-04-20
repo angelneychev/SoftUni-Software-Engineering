e02cbe1101263251577d56fd465672c5ce162449
Angel Neychev <angel.neychev@gmail.com>
2019-04-20 10:52:42 +0300

    First Steps In Coding

---
+++ Programming Basics/First Steps In Coding/03. Nums 1-20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Nums_1_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write(H + w);
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+            //Console.WriteLine(11);
+            //Console.WriteLine(12);
+
+            for (int count = 1; count <= 20; count++)
+            {
+                Console.WriteLine(count);
+            }
+
+        }
+    }
+    }
+}
---
+++ Programming Basics/First Steps In Coding/04. Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}

b5a1468e78918a6a13e544d660ecefeaac3d379e
Angel Neychev <angel.neychev@gmail.com>
2019-06-09 16:02:12 +0300

    Stacks and Queues - Exercise

--- C# Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/FashionBoutique.cs
+++ C# Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/FashionBoutique.cs
@@ -8,7 +8,41 @@ namespace _05._Fashion_Boutique
     {
         static void Main()
         {
-            
+            var input = Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var clothes = new Stack<int>(input);
+
+            int packCapacity = int.Parse(Console.ReadLine());
+            int startCapacity = packCapacity;
+            int totalPack = 1;
+            while (clothes.Count > 0)
+            {
+                int countClothes = clothes.Pop();
+                int nalCapacity = packCapacity - countClothes;
+
+                if (nalCapacity > 0)
+                {
+                    packCapacity -= countClothes;
+                }
+                else if (nalCapacity == 0)
+                {
+                    packCapacity = startCapacity;
+                    if (clothes.Count > 0)
+                    {
+                        totalPack++;
+                    }
+                }
+                else if (nalCapacity < 0)
+                {
+                    totalPack++;
+                    packCapacity = startCapacity;
+                    packCapacity -= countClothes;
+                }
+
+            }
+
+            Console.WriteLine(totalPack);
         }
     }
 }

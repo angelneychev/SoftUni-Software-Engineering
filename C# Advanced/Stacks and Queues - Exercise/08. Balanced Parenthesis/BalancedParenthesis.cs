0920fa3059f009625b18e2a17dc62109945bf180
Angel Neychev <angel.neychev@gmail.com>
2019-06-10 14:59:18 +0300

    Stacks and Queues - Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/08. Balanced Parenthesis/BalancedParenthesis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    public class BalancedParenthesis
+    {
+        static void Main()
+        {
+            Stack<char> stackOfParenteses = new Stack<char>();
+
+            char[] input = Console.ReadLine().ToCharArray();
+
+            char[] openParenteses = new[] {'(', '{', '['};
+
+            bool isValid = true;
+
+            foreach (var item in input)
+            {
+                if (openParenteses.Contains(item))
+                {
+                    stackOfParenteses.Push(item);
+                    continue;
+                }
+
+                if (stackOfParenteses.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (stackOfParenteses.Peek() == '(' && item == ')')
+                {
+                    stackOfParenteses.Pop();
+                }
+                else if (stackOfParenteses.Peek() == '{' && item == '}')
+                {
+                    stackOfParenteses.Pop();
+                }
+
+                else if (stackOfParenteses.Peek() == '[' && item == ']')
+                {
+                    stackOfParenteses.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Queues - Exercise/09. Simple Text Editor/SimpleTextEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Simple_Text_Editor
+{
+    public class SimpleTextEditor
+    {
+        static void Main()
+        {
+            Stack<string> stackOfText = new Stack<string>();
+            string text = string.Empty;
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (input[0] == "1")
+                {
+                    stackOfText.Push(text);
+                    text += input[1];
+                }
+                else if (input[0] == "2")
+                {
+                    int index = int.Parse(input[1]);
+                    stackOfText.Push(text);
+                    text = text.Substring(0, text.Length - index);
+                }
+                else if (input[0] == "3")
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (input[0] == "4")
+                {
+                    text = stackOfText.Pop();
+                }
+            }
+        }
+    }
+}

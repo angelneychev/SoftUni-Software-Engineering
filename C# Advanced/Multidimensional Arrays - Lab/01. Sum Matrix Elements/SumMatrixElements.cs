467f23aa80e8b6d9f128c0761ce7b6e8a8c55864
Angel Neychev <angel.neychev@gmail.com>
2019-06-11 06:59:45 +0300

    Multidimensional Arrays - Lab

---
+++ C# Advanced/Multidimensional Arrays - Lab/01. Sum Matrix Elements/SumMatrixElements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Matrix_Elements
+{
+    public class SumMatrixElements
+    {
+        static void Main()
+        {
+            //int rows = int.Parse(Console.ReadLine());
+            //int cols = int.Parse(Console.ReadLine());
+            int[] size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = size[0];
+            int cols = size[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}

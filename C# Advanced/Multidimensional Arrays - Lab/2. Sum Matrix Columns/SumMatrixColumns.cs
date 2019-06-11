4999a3fe68a68c1b8bc9088e1b9f655abe2a8f40
Angel Neychev <angel.neychev@gmail.com>
2019-06-11 07:37:06 +0300

    Multidimensional Arrays - Lab

---
+++ C# Advanced/Multidimensional Arrays - Lab/2. Sum Matrix Columns/SumMatrixColumns.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sum_Matrix_Columns
+{
+   public class SumMatrixColumns
+    {
+        static void Main()
+        {
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = size[0];
+            int cols = size[1];
+            int[,] matrix = new int[rows,cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
+
+            
+
+        }
+    }
+}

9fc8f1cf915b282c4e644f9d8123af75ec5cbc6f
Angel Neychev <angel.neychev@gmail.com>
2019-06-11 16:26:45 +0300

    Multidimensional Arrays - Lab

---
+++ C# Advanced/Multidimensional Arrays - Lab/06. Jagged-Array Modification/JaggedArrayModification.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Jagged_Array_Modification
+{
+    public class JaggedArrayModification
+    {
+        static void Main()
+        {
+            //подавам колко редава да има назъбения масив
+            int rows = int.Parse(Console.ReadLine());
+            int[][] arr = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                //всеки ред си е отделен масив
+                arr[row] = new int[currentRow.Length];
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    arr[row][col] = currentRow[col];
+                }
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command.ToLower() == "end")
+                {
+                    break;
+                }
+
+                var commandParts = command.Split();
+                string commandOperator = commandParts[0];
+                var commandRow = int.Parse(commandParts[1]);
+                var commandCol = int.Parse(commandParts[2]);
+                var value = int.Parse(commandParts[3]);
+                //проверка за валидни кординати
+                if (commandRow < 0 || commandRow >= arr.Length || commandCol < 0 ||
+                    commandCol >= arr[commandRow].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (commandOperator == "Add")
+                {
+                    arr[commandRow][commandCol] += value;
+                }
+                else if (commandOperator == "Subtract")
+                {
+                    arr[commandRow][commandCol] -= value;
+                }
+            }
+
+            // така можем да принтираме само назъбени масиви
+            foreach (var row in arr)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
---
+++ C# Advanced/Multidimensional Arrays - Lab/07. Pascal Triangle/PascalTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Pascal_Triangle
+{
+    public class PascalTriangle
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] pascalArr = new int[rows][];
+            pascalArr[0] = new int[1];
+            pascalArr[0][0] = 1;
+
+            for (int row = 0; row < rows; row++)
+            {
+                pascalArr[row] = new int[row + 1];
+                pascalArr[row][0] = 1;
+                pascalArr[row][pascalArr[row].Length - 1] = 1;
+                for (int col = 1; col < pascalArr[row].Length - 1; col++)
+                {
+                    pascalArr[row][col] += pascalArr[row - 1][col]
+                                           + pascalArr[row - 1][col - 1];
+                }
+            }
+
+            foreach (var row in pascalArr)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}

c7bb994018530d04aa8b6bc5c561de33397dfb27
Angel Neychev <angel.neychev@gmail.com>
2019-06-16 08:57:27 +0300

    C# Advanced Exam - 24 February 2019

---
+++ C# Advanced/CSharp Advanced Exam - 24 February 2019/01. Club Party/ClubParty.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Club_Party
+{
+   public class ClubParty
+    {
+        static void Main()
+        {
+            int hallsMaxCapacity = int.Parse(Console.ReadLine());
+            Stack<string> symbols = new Stack<string>(Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            List<string> halls = new List<string>();
+            List<int> people = new List<int>();
+
+            while (symbols.Count != 0)
+            {
+                string currentSymbols = symbols.Pop();
+
+                if (int.TryParse(currentSymbols, out int capacityNeeded))
+                {
+                    if (halls.Count != 0)
+                    {
+                        if (people.Sum() + capacityNeeded <= hallsMaxCapacity)
+                        {
+                            people.Add(capacityNeeded);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{halls[0]} -> {string.Join(", ", people)}");
+                            halls.RemoveAt(0);
+                            people.Clear();
+
+                            if (halls.Count != 0)
+                            {
+                                people.Add(capacityNeeded);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    halls.Add(currentSymbols);
+                }
+            }
+
+        }
+    }
+}

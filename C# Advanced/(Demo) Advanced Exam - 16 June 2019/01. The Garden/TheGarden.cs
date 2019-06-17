dc91d3d2ffa4cf13abbddd8d1ef0d37e6f03d132
Angel Neychev <angel.neychev@gmail.com>
2019-06-17 11:39:52 +0300

    (Demo) C# Advanced Exam - 16 June 2019

---
+++ C# Advanced/(Demo) Advanced Exam - 16 June 2019/01. The Garden/TheGarden.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._The_Garden
+{
+    public class TheGarden
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            string[][] matrix = new string[rows][];
+
+            for (int col = 0; col < rows; col++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                matrix[col] = input;
+            }
+
+            string inputCommand = Console.ReadLine();
+            int countOfCarrots = 0;
+            int countOfPotatos = 0;
+            int countOfCucmbers = 0;
+            int countOfHarmed = 0;
+
+            while (inputCommand != "End of Harvest")
+            {
+                string[] tokens = inputCommand.Split(" ").ToArray();
+
+                string command = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+                if (command == "Harvest")
+                {
+                    if (isInside(matrix, row, col) && matrix[row][col] != " ")
+                    {
+                        switch (matrix[row][col])
+                        {
+                            case "P":
+                                countOfPotatos++;
+                                break;
+                            case "C":
+                                countOfCarrots++;
+                                break;
+                            case "L":
+                                countOfCucmbers++;
+                                break;
+                        }
+
+                        matrix[row][col] = " ";
+                    }
+                }
+                else if (command == "Mole")
+                {
+                    string direction = tokens[3];
+                    while (isInside(matrix, row, col) && matrix[row][col] !=" ")
+                    { 
+                        switch (direction)
+                        {
+                            case "up":
+                                matrix[row][col] = " ";
+                                countOfHarmed++;
+                                row -= 2;
+                                break;
+
+                            case "down":
+                                matrix[row][col] = " ";
+                                countOfHarmed++;
+                                row += 2;
+                                break;
+
+                            case "left":
+                                matrix[row][col] = " ";
+                                countOfHarmed++;
+                                col -= 2;
+                                break;
+
+                            case "right":
+                                matrix[row][col] = " ";
+                                countOfHarmed++;
+                                col += 2;
+                                break;
+                        }
+                    }
+                }
+
+                inputCommand = Console.ReadLine();
+            }
+
+            foreach (var item in matrix)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+            Console.WriteLine($"Carrots: {countOfCarrots}");
+            Console.WriteLine($"Potatoes: {countOfPotatos}");
+            Console.WriteLine($"Lettuce: {countOfCucmbers}");
+            Console.WriteLine($"Harmed vegetables: {countOfHarmed}");
+
+        }
+
+        private static bool isInside(string[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+    }
+}
---
+++ C# Advanced/(Demo) Advanced Exam - 16 June 2019/02. Make a Salad/MakeSalad.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Make_a_Salad
+{
+   public class MakeSalad
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+        
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<string> salads = new Queue<string>(input);
+            Stack<int> calories = new Stack<int>(numbers);
+            Queue<int> finishedSalads = new Queue<int>();
+            int currentDiff = 0;
+            int sumCalorie = 0;
+            int saladCalorie = 0;
+            if (calories.Count >0)
+            {
+                saladCalorie = calories.Peek();
+            }
+            
+
+            while (salads.Count !=0)
+            {
+                if (calories.Count == 0)
+                {
+                    break;
+                }
+                string vegetable = salads.Peek();
+                switch (vegetable)
+                    {
+                        case "tomato":
+                            sumCalorie += 80;
+                            vegetable = salads.Dequeue();
+                            break;
+                        case "carrot":
+                            sumCalorie += 136;
+                            vegetable = salads.Dequeue();
+                            break;
+                        case "lettuce":
+                            sumCalorie += 109;
+                            vegetable = salads.Dequeue();
+                            break;
+                        case "potato":
+                            sumCalorie += 215;
+                            vegetable = salads.Dequeue();
+                            break;
+                    }
+
+                currentDiff = saladCalorie - sumCalorie;
+                sumCalorie = 0;
+                saladCalorie = currentDiff;
+                if (currentDiff < 0)
+                {
+                    finishedSalads.Enqueue(calories.Pop());
+                    if (calories.Count>0)
+                    {
+                        saladCalorie += calories.Peek();
+                    }
+                    
+                }
+            }
+            Console.WriteLine(string.Join(" ", finishedSalads));
+            if (calories.Count >0)
+            {
+                Console.WriteLine(string.Join(" ",calories));
+            }
+            else if (salads.Count >0)
+            {
+                Console.WriteLine(string.Join(" ", salads));
+            }
+        }
+    }
+}

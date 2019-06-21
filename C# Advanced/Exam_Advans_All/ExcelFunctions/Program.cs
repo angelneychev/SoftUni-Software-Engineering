5408278dc95da80f2790ae8def4a26f21bb357aa
Angel Neychev <angel.neychev@gmail.com>
2019-06-21 23:40:50 +0300

    C# Advanced Exam

---
+++ C# Advanced/Exam_Advans_All/ExcelFunctions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace ExcelFunctions
+{
+    public class ExcelFunctions
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            string[][] matrix = new string[rows][];
+
+            for (int col = 0; col < rows; col++)
+            {
+                string[] input = Console.ReadLine().Split(", ").ToArray();
+                matrix[col] = input;
+            }
+
+            string[] commands = Console.ReadLine().Split().ToArray();
+            string command = commands[0];
+            string header = commands[1];
+
+            int headerIndex = Array.IndexOf(matrix[0], header);
+
+            if (command == "hide")
+            {
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    List<string> printList = new List<string>(matrix[row]);
+
+                    printList.RemoveAt(headerIndex);
+                    Console.WriteLine(string.Join(" | ", printList));
+                }
+            }
+            else if (command == "sort")
+            {
+                string[] headerRow = matrix[0];
+                Console.WriteLine(string.Join(" | ", headerRow));
+                // сортирам матрицара
+                matrix = matrix.OrderBy(x => x[headerIndex]).ToArray();
+                foreach (var row in matrix)
+                {
+                    if (row != headerRow)
+                    {
+                        Console.WriteLine(string.Join(" | ", row));
+                    }
+                }
+            }
+            else if (command == "filter")
+            {
+                string valueCol = commands[2];
+                string[] headerRow = matrix[0];
+                Console.WriteLine(string.Join(" | ", matrix[0]));
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    if (matrix[row][headerIndex] == valueCol)
+                    {
+                        Console.WriteLine(string.Join(" | ", matrix[row]));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exam_Advans_All/Socks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leftInput = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            var rightInput = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> leftSock =new Stack<int>(leftInput);
+            Queue<int> rightSock = new Queue<int>(rightInput);
+            var pair = new List<int>();
+
+            while (leftSock.Count >0 && rightSock.Count > 0)
+            {
+                int left = leftSock.Peek();
+                int right = rightSock.Peek();
+
+                if (left > right)
+                {
+                    int sum = left + right;
+                    pair.Add(sum);
+                    leftSock.Pop();
+                    rightSock.Dequeue();
+
+                }
+                else if (left < right)
+                {
+                    leftSock.Pop();
+                }
+                else if (left == right)
+                {
+                    rightSock.Dequeue();
+                    left++;
+                    leftSock.Pop();
+                    leftSock.Push(left);
+                }
+
+            }
+
+            int maxPair = pair.Max();
+            Console.WriteLine(maxPair);
+            Console.WriteLine(string.Join(" ", pair));
+        }
+    }
+}

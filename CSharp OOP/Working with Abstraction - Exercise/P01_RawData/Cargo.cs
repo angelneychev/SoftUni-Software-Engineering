d144d809edcb4c07d868959dd0d661fdc156d4ef
Angel Neychev <angel.neychev@gmail.com>
2019-06-30 15:43:00 +0300

    100/100

---
+++ CSharp OOP/Working with Abstraction - Exercise/P01_RawData/Cargo.cs
@@ -0,0 +1,18 @@
+﻿namespace P01_RawData
+{
+   public class Cargo
+   {
+        //int cargoWeight, string cargoType
+        private string type;
+        private int weight;
+
+        // правим конструктор задължително за да ги сетнем
+        public Cargo(int weight, string type)
+        {
+            this.weight = weight;
+            this.Type = type;
+        }
+
+        public string Type { get; private set; }
+   }
+}

44b332cb6df34ac18775124bafb1d03f39dd2112
Angel Neychev <angel.neychev@gmail.com>
2019-06-30 08:56:46 +0300

    Working with Abstraction - Exercise

---
+++ C# Advanced/CSharp Advanced Exam - 23 June 2019/01. Spaceship Crafting/SpaceshipCrafting.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Spaceship_Crafting
+{
+    public class SpaceshipCrafting
+    {
+        static void Main()
+        {
+            int[] inputLiquid = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] inputItem = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> liquidQueue = new Queue<int>(inputLiquid);
+            Stack<int> itemStack = new Stack<int>(inputItem);
+            Dictionary<string, int> allMaterial = new Dictionary<string, int>
+            {
+                {"Glass", 0}, {"Aluminium", 0}, {"Lithium", 0}, {"Carbon fiber", 0}
+            };
+
+            while (liquidQueue.Count > 0 && itemStack.Count > 0)
+            {
+                int liquid = liquidQueue.Peek();
+                int item = itemStack.Peek();
+                int sum = liquid + item;
+
+                if (sum == 25)
+                {
+                    allMaterial["Glass"] += 1;
+                    liquidQueue.Dequeue();
+                    itemStack.Pop();
+                }
+                else if (sum == 50)
+                {
+                    allMaterial["Aluminium"] += 1;
+                    liquidQueue.Dequeue();
+                    itemStack.Pop();
+                }
+                else if (sum == 75)
+                {
+                    allMaterial["Lithium"] += 1;
+                    liquidQueue.Dequeue();
+                    itemStack.Pop();
+                }
+                else if (sum == 100)
+                {
+                    allMaterial["Carbon fiber"] += 1;
+                    liquidQueue.Dequeue();
+                    itemStack.Pop();
+                }
+                else
+                {
+                    liquidQueue.Dequeue();
+                    itemStack.Pop();
+                    item += 3;
+                    itemStack.Push(item);
+                }
+            }
+            int count = 0;
+            foreach (var kvp in allMaterial.OrderBy(x => x.Key))
+            {
+                if (kvp.Value > 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count >= 4)
+            {
+                Console.WriteLine("Wohoo! You succeeded in building the spaceship!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to build the spaceship.");
+
+            }
+            if (liquidQueue.Count > 0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquidQueue)}");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: none");
+            }
+            if (itemStack.Count > 0)
+            {
+                Console.WriteLine($"Physical items left: {string.Join(", ", itemStack)}");
+            }
+            else
+            {
+                Console.WriteLine($"Physical items left: none");
+            }
+            foreach (var kvp in allMaterial.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharp Advanced Exam - 23 June 2019/03SpaceStationEstablishment/SpaceStationEstablishment.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03SpaceStationEstablishment
+{
+   public class PSpaceStationEstablishment
+    {
+        static char[][] galaxy;
+        static int spaceshipRow;
+        static int spaceshipCol;
+        static int firstBlackholeRow = -1;
+        static int firstBlackholeCol = -1;
+        static int secondBlackholeRow;
+        static int secondBlackholeCol;
+        static int starsCount;
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            galaxy = new char[rows][];
+
+            InitiliazeGalaxy();
+            SpaceshipMove();
+
+            if (starsCount >= 50)
+            {
+                Console.WriteLine("Good news! Stephen succeeded in collecting enough star power!");
+                Console.WriteLine($"Star power collected: {starsCount}");
+                PrintGalaxy();
+            }
+            else
+            {
+                Console.WriteLine("Bad news, the spaceship went to the void.");
+                Console.WriteLine($"Star power collected: {starsCount}");
+                PrintGalaxy();
+            }
+        }
+
+        private static void PrintGalaxy()
+        {
+            foreach (var row in galaxy)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        private static void SpaceshipMove()
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "up" && IsInside(spaceshipRow - 1, spaceshipCol))
+                {
+                    spaceshipRow--;
+                }
+                else if (command == "down" && IsInside(spaceshipRow + 1, spaceshipCol))
+                {
+                    spaceshipRow++;
+                }
+                else if (command == "left" && IsInside(spaceshipCol - 1, spaceshipRow))
+                {
+                    spaceshipCol--;
+                }
+                else if (command == "right" && IsInside(spaceshipCol + 1, spaceshipRow))
+                {
+                    spaceshipCol++;
+                }
+                else
+                {
+                    break;
+                }
+
+                if (char.IsDigit(galaxy[spaceshipRow][spaceshipCol]))
+                {
+                    starsCount += galaxy[spaceshipRow][spaceshipCol] - '0';
+                    galaxy[spaceshipRow][spaceshipCol] = '-';
+
+                    if (starsCount >= 50)
+                    {
+                        galaxy[spaceshipRow][spaceshipCol] = 'S';
+                        break;
+                    }
+                }
+                else if (galaxy[spaceshipRow][spaceshipCol] == 'O')
+                {
+                    spaceshipRow = secondBlackholeRow;
+                    spaceshipCol = secondBlackholeCol;
+
+                    galaxy[firstBlackholeRow][firstBlackholeCol] = '-';
+                    galaxy[secondBlackholeRow][secondBlackholeCol] = '-';
+                }
+            }
+        }
+
+        // How can I be so stupid not to set the initial blackholes dimensions ?!
+        private static void InitiliazeGalaxy()
+        {
+            for (int row = 0; row < galaxy.Length; row++)
+            {
+                char[] values = Console.ReadLine().ToCharArray();
+                galaxy[row] = new char[values.Length];
+
+                for (int col = 0; col < values.Length; col++)
+                {
+                    galaxy[row][col] = values[col];
+
+                    if (galaxy[row][col] == 'S')
+                    {
+                        spaceshipRow = row;
+                        spaceshipRow = col;
+                        galaxy[row][col] = '-';
+                    }
+                    else if (galaxy[row][col] == 'O' && firstBlackholeRow == -1 && firstBlackholeCol == -1)
+                    {
+                        firstBlackholeRow = row;
+                        firstBlackholeCol = col;
+                    }
+                    else if (galaxy[row][col] == 'O' && firstBlackholeRow != -1 && firstBlackholeCol != -1)
+                    {
+                        secondBlackholeRow = row;
+                        secondBlackholeCol = col;
+                    }
+                }
+            }
+        }
+
+        private static bool IsInside(int desiredRow, int desiredCol)
+        {
+            return desiredRow >= 0 && desiredRow < galaxy.Length
+                && desiredCol >= 0 && desiredCol < galaxy[desiredRow].Length;
+        }
+    }
+}

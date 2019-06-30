02b22a81d720e9aefd290ec92a08d8a96c12dc60
Angel Neychev <angel.neychev@gmail.com>
2019-06-30 08:57:21 +0300

    Exams

---
+++ C# Advanced/Exam_Advans_All/SoftUni Parking/SoftUniParking/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make
+        {
+            get => this.make;
+            set => this.make = value;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public int HorsePower
+        {
+            get => this.horsePower;
+            set => this.horsePower = value;
+        }
+
+        public string RegistrationNumber
+        {
+            get => this.registrationNumber;
+            set => this.registrationNumber = value;
+        }
+
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+    }
+}

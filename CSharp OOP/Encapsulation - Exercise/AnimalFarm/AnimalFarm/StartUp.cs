0013edb8172c97c4a3e1c9afdf4428667b5e773b
Angel Neychev <angel.neychev@gmail.com>
2019-07-08 11:13:48 +0300

    100/100

---
+++ CSharp OOP/Encapsulation - Exercise/AnimalFarm/AnimalFarm/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AnimalFarm.Models;
+
+namespace AnimalFarm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            try
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay} eggs per day.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}

a43598deca2e42ab05f406a83021e9711c4c69b3
Angel Neychev <angel.neychev@gmail.com>
2019-07-08 10:08:17 +0300

    100/100

---
+++ CSharp OOP/Encapsulation - Exercise/Class Box Data/Box.cs
@@ -0,0 +1,77 @@
+﻿namespace Class_Box_Data
+{
+    using System;
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Lenght = length;
+        }
+        public double Height
+        {
+            get { return this.height; }
+            set {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get { return this.width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Lenght
+        {
+            get { return this.length; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Lenght cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public string SurfaceArea()
+        {
+            double surfaceArea = 2 * length * width + 2 * length * height + 2 * width * height;
+
+
+            return $"Surface Area - {surfaceArea:F2}";
+
+        }
+
+        public string LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * length * height + 2 * width * height;
+            return $"Lateral Surface Area - {lateralSurfaceArea:f2}";
+        }
+
+        public string Volume()
+        {
+            double volume = height * width * length;
+            return $"Volume - {volume:f2}";
+        }
+    }
+}
---
+++ CSharp OOP/Encapsulation - Exercise/Class Box Data/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace Class_Box_Data
+{
+    using System;
+    public class StatUp
+    {
+        static void Main()
+        {
+           // Box box = null;
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box = new Box(length,width,height);
+
+                Console.WriteLine(box.SurfaceArea());
+                Console.WriteLine(box.LateralSurfaceArea());
+                Console.WriteLine(box.Volume());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

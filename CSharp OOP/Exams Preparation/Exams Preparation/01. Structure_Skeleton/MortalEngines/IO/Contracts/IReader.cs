12908266842702d0fc43c30b1d8ef740705c2e63
Angel Neychev <angel.neychev@gmail.com>
2019-07-25 23:53:18 +0300

    C# OOP Exam - 14 April 2019 - 200/160

---
+++ CSharp OOP/Exams Preparation/Exams Preparation/01. Structure_Skeleton/MortalEngines/IO/Contracts/IReader.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Windows.Input;
+
+namespace MortalEngines.IO.Contracts
+{
+    using MortalEngines.Core.Contracts;
+    using System.Collections.Generic;
+
+    public interface IReader
+    {
+        IList<ICommand> ReadCommands();
+    }
+}

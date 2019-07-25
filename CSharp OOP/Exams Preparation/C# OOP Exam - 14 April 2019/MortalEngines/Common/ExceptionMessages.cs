9237ccd6be080a13d6f5abc2633bd78a2d59dca8
Angel Neychev <angel.neychev@gmail.com>
2019-07-26 00:08:31 +0300

    C# OOP Exam - 14 April 2019

---
+++ CSharp OOP/Exams Preparation/C# OOP Exam - 14 April 2019/MortalEngines/Common/ExceptionMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace MortalEngines.Common
+{
+    public static class ExceptionMessages
+    {
+        public const string NullMachineName = "Machine name cannot be null or empty.";
+        public const string NullPilot = "Pilot cannot be null.";
+
+        public const string NullTarget = "Target cannot be null";
+
+        public const string NullPilotName = "Pilot name cannot be null or empty string.";
+
+        public const string NullMachine = "Null machine cannot be added to the pilot.";
+    }
+}
---
+++ CSharp OOP/Exams Preparation/C# OOP Exam - 14 April 2019/MortalEngines/Entities/Model/Fighter.cs
@@ -0,0 +1,54 @@
+﻿namespace MortalEngines.Entities.Model
+{
+    using System.Text;
+    using MortalEngines.Entities.Contracts;
+
+    public class Fighter : BaseMachine, IFighter
+    {
+        //Has 200 initial health points.
+        private const int InitialHealthPoints = 200;
+        private const int ActivatedAttackPoints = 50;
+        private const int DeactivatedDeffencePoints = 25;
+
+        // AggressiveMode – boolo	true by default
+        public Fighter(string name, double attackPoints, double defensePoints)
+            : base(name,
+                InitialHealthPoints,
+                attackPoints + ActivatedAttackPoints,
+                defensePoints - DeactivatedDeffencePoints)
+        {
+            // AggressiveMode – boolo	true by default
+            this.AggressiveMode = true;
+        }
+        public bool AggressiveMode { get; private set; }
+
+        public void ToggleAggressiveMode()
+        {
+            if (AggressiveMode == false)
+            {
+                this.AggressiveMode = true;
+                this.AttackPoints += ActivatedAttackPoints;
+                this.DefensePoints -= DeactivatedDeffencePoints;
+            }
+            else
+            {
+                this.AggressiveMode = false;
+                this.AttackPoints -= ActivatedAttackPoints;
+                this.DefensePoints += DeactivatedDeffencePoints;
+            }
+        }
+
+        public override string ToString()
+        {
+            //" *Aggressive: {ON/OFF}"
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+
+            sb.AppendLine($" *Aggressive: {(this.AggressiveMode == true ? "ON" : "OFF")}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

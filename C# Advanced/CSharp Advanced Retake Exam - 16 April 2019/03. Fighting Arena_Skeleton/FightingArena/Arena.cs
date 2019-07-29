9b87f6c7a7cbed9975de2def27e9f6514073e712
Angel Neychev <angel.neychev@gmail.com>
2019-07-29 12:02:34 +0300

    03. Fighting Arena 100/100

---
+++ C# Advanced/CSharp Advanced Retake Exam - 16 April 2019/03. Fighting Arena_Skeleton/FightingArena/Arena.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FightingArena
+{
+    public class Arena
+    {
+        private List<Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            this.Name = name;
+            this.gladiators = new List<Gladiator>();
+        }
+
+        public string Name { get; set; }
+        public int Count => this.gladiators.Count;
+
+        //•	Method Add(Gladiator gladiator) – adds an gladiator to the arena.
+        public void Add(Gladiator gladiator)
+        {
+            this.gladiators.Add(gladiator);
+        }
+        //•	Method Remove(string name) – removes an gladiator by given name.
+        public void Remove(string name)
+        {
+            Gladiator gladiatorName = this.gladiators.FirstOrDefault(x => x.Name == name);
+            this.gladiators.Remove(gladiatorName);
+
+        }
+        //•	Method GetGladitorWithHighestStatPower() – returns the Gladiator which has the highest stat.
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            int highestStatPower = this.gladiators.Max(x => x.GetStatPower());
+            return this.gladiators.FirstOrDefault(x => x.GetStatPower() == highestStatPower);
+        }
+        //•	Method GetGladitorWithHighestWeaponPower() – returns the Gladiator which poses the weapon with the highest power.
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            int highestWeaponPower = this.gladiators.Max(x => x.GetWeaponPower());
+            return this.gladiators.FirstOrDefault(x => x.GetWeaponPower() == highestWeaponPower);
+        }
+        //•	Method GetGladitorWithHighestTotalPower() – returns the Gladiator which has the highest total power.
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            int highestTotalPower = this.gladiators.Max(x => x.GetTotalPower());
+            return this.gladiators.FirstOrDefault(x => x.GetTotalPower() == highestTotalPower);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.gladiators.Count} gladiators are participating.";
+        }
+    }
+}
---
+++ C# Advanced/CSharp Advanced Retake Exam - 16 April 2019/03. Fighting Arena_Skeleton/FightingArena/Gladiator.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace FightingArena
+{
+    public class Gladiator
+    {
+        public string Name { get; set; }
+        public Stat Stat { get; set; }
+        public Weapon Weapon { get; set; }
+
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            this.Name = name;
+            this.Stat = stat;
+            this.Weapon = weapon;
+        }
+        public int GetTotalPower()
+        {
+            //GetTotalPower(): int – return the sum of the stat properties plus the sum of the weapon properties.
+            return GetWeaponPower() + GetStatPower();
+        }
+        public int GetWeaponPower()
+        {
+            //GetWeaponPower(): int - return the sum of the weapon properties.
+            return this.Weapon.Sharpness
+                   + this.Weapon.Size
+                   + this.Weapon.Solidity;
+        }
+        public int GetStatPower()
+        {
+            //GetStatPower(): int - return the sum of the stat properties.
+            return this.Stat.Agility
+                   + this.Stat.Flexibility
+                   + this.Stat.Intelligence
+                   + this.Stat.Skills
+                   + this.Stat.Strength;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            //"[Gladiator name] - [Gladiator total power]"
+            sb.AppendLine($"{this.Name} - {this.GetTotalPower()}");
+            // "  Weapon Power: [Gladiator weapon power]"
+            sb.AppendLine($"Weapon Power: {this.GetWeaponPower()}");
+            // "  Stat Power: [Gladiator stat power]"
+            sb.AppendLine($"Stat Power: {this.GetStatPower()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

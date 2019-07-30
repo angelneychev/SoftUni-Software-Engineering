4749ae6cde7b2054c7385744ef9323ed0d86ab3b
Angel Neychev <angel.neychev@gmail.com>
2019-07-30 14:47:26 +0300

    0/150

---
+++ CSharp OOP/Exams Preparation/C# OOP Retake Exam - 18 April 2019/02. Structure/PlayersAndMonsters/Models/Cards/TrapCard.cs
@@ -0,0 +1,15 @@
+﻿namespace PlayersAndMonsters.Models.Cards
+{
+    public class TrapCard : Card
+    {
+        //Has 120 damage points and 5 health points.
+        private const int DefaultDamagePoints = 5;
+        private const int DefaultHealthPoints = 80;
+
+        //Constructor should take the following values upon initialization: string name
+        public TrapCard(string name) 
+            : base(name, DefaultDamagePoints, DefaultHealthPoints)
+        {
+        }
+    }
+}
